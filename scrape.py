91c1d49eeca4a8eebe78515f4abb9131cd3d14e2
sm_peterson@yahoo.com <gi6h259b>
2017-02-19 20:56:28 -0600

    4

--- scrape.py
+++ scrape.py
@@ -2,10 +2,10 @@ from lxml import html
 import requests
 import json
 
-lookforwhat = "us-west-1"
+lookforwhat = "cn-north-1"
 page = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json')
 tree = html.fromstring(page.content)
 parsed_json = json.loads(page.text)
 for each in parsed_json['prefixes']:
     if each['region'] == lookforwhat:
-        print (each['ip_prefix'])
+        print(each['ip_prefix'])
